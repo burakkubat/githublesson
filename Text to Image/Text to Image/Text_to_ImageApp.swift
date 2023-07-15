ef9df5f76a91b1b7fa01e3cad563fb9f9ef67569
burak.kubat <burakkubat@Buraks-MacBook-Air.local>
2023-07-15 20:25:52 +0300

    1234 yazildi

---
+++ Text to Image/Text to Image/Text_to_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_to_ImageApp.swift
+//  Text to Image
+//
+//  Created by Burak Kubat on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Text_to_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
